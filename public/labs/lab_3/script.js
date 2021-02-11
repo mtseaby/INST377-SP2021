035f9b79b21004e13b33633c933db55319f0663d
Mtseaby <78050198+mtseaby@users.noreply.github.com>
2021-02-11 16:45:26 -0500

    Lab3 completion v1.0 (added HTML, JS, and CSS)

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,26 @@
 /* Put your javascript in here */
+
+const btnPrev = document.querySelector('button.arrow.prev');
+    const btnNext = document.querySelector('button.arrow.next');
+    const ul = document.querySelector('div.galery > ul');
+    const liWidth = document.querySelector('div.galery > ul > li').clientWidth;
+    const limit = ul.querySelectorAll('img').length * liWidth; 
+    
+    const count = 3;
+    let position = 0;
+    
+    console.log(ul); 
+    
+    const scrollFn = (isNext) => {
+      const off = position + (count * (isNext ? liWidth : -(liWidth)));
+      console.log(off);
+      if(off < 0 || off > limit) {
+        return;
+      }
+      
+      position = off;
+      ul.style.transform = `translateX(${-(position)}px)`;
+    };
+    
+    btnPrev.onclick = () => scrollFn(false);
+    btnNext.onclick = () => scrollFn(true);
